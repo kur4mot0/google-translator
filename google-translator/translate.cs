527deda3da968f1d5a02ad82c7fc173a54571b67
Kuramoto <kuramoto@outlook.com>
2019-06-20 21:31:35 +0100

    Google translator for Docker.

--- google-translator/translate.cs
+++ google-translator/translate.cs
@@ -7,13 +7,16 @@ using TranslationsResource = Google.Apis.Translate.v2.Data.TranslationsResource;
 
 namespace google_translator
 {
-    class Program
+    class Translate
     {
+        private string key;
+        private string lang;
+
         static void Main(string[] args)
         {    
             try
             {
-                new Program().Run(args[0]).Wait();
+                new Translate().Run(args[0]).Wait();
             }
             catch (AggregateException ex)
             {
@@ -26,21 +29,43 @@ namespace google_translator
         
         private async Task Run(string srcText)
         {
-            var key = Environment.GetEnvironmentVariable("key");           
+
+            getEnvironment();
+
             var service = new TranslateService(new BaseClientService.Initializer()
                 {
                     ApiKey = key,
                     ApplicationName = "Translate API Sample"
                 });
 
-            var response = await service.Translations.List(srcText, "Pt-Br").ExecuteAsync();
-            var translations = new List<string>();
+            var response = await service.Translations.List(srcText, lang).ExecuteAsync();            
 
             foreach (TranslationsResource translation in response.Translations)
             {
-                translations.Add(translation.TranslatedText);
                 Console.WriteLine(translation.TranslatedText);
             }            
-        }        
+        }
+
+        public void getEnvironment()
+        {
+            if(!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("KEY")))
+            {
+                key = Environment.GetEnvironmentVariable("KEY");
+            }
+            else
+            {
+                Console.WriteLine("Please, set environment for API Key from console.developers.googlec.com");
+            }
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("LANG")))
+            {
+                lang = Environment.GetEnvironmentVariable("LANG");
+            }
+            else
+            {
+                lang = "Pt";
+            }
+        }
+
     }
 }
